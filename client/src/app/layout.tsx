319ff9cab499a2645bca33ece06e90846f566d1b
LuaanNguyen <luanthiennguyen2003@gmail.com>
2024-09-07 15:19:48 -0700

    Added theme-provider.tsx

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { ThemeProvider } from "./components/theme-provider";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -13,7 +14,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
