0963235a5b6b5b2c81209bfa029978865f7cd4ce
LuaanNguyen <luanthiennguyen2003@gmail.com>
2024-09-07 17:25:53 -0700

    Added Map Components

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,16 +1,13 @@
 "use client";
 
+import Map from "./Map";
+
 export default function Home() {
   return (
-    <main className="grid grid-cols-4">
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
-      <div>4</div>
-
-      {/* <button type="button" onClick={toggleTheme}>
-        set theme
-      </button> */}
+    <main className="grid grid-cols-4 h-screen">
+      <div className="border-r-2 h-[100%]">1</div>
+      <Map />
+      <div className="border-l-2 h-[100%]">4</div>
     </main>
   );
 }
