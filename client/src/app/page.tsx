1f90a2fcc47138c18c3fac540a624f0056ccd5c6
LuaanNguyen <luanthiennguyen2003@gmail.com>
2024-09-11 14:37:02 -0700

    Added Loader Component 🍳

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,7 +3,9 @@
 import dynamic from "next/dynamic";
 const Map = dynamic(() => import("./Map"), {
   ssr: false,
-  loading: () => <p>Loading map...</p>,
+  loading: () => (
+    <div className="relative  mx-auto my-auto inset-0 w-12 h-12 rounded-full animate-spin border-8 border-dashed border-[#6B8E23] border-t-transparent"></div>
+  ),
 });
 import Info from "./my-components/info";
 
